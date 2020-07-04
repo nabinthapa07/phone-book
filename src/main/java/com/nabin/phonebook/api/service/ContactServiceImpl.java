8d9c14dd32b30711d2707df4ddc4a304845ed054
nabinthapa07 <nabin.thapant51@gmail.com>
2020-07-03 19:12:16 -0500

    updated

--- src/main/java/com/nabin/phonebook/api/service/ContactServiceImpl.java
+++ src/main/java/com/nabin/phonebook/api/service/ContactServiceImpl.java
@@ -2,17 +2,26 @@ package com.nabin.phonebook.api.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nabin.phonebook.api.dao.ContactDtlsRepository;
+import com.nabin.phonebook.api.dao.ContactEntity;
 import com.nabin.phonebook.api.model.Contact;
 
 @Service
 public class ContactServiceImpl implements ContactService{
+	
+	@Autowired
+	private ContactDtlsRepository contactDtlRepository;
 
 	@Override
 	public boolean saveContact(Contact contact) {
-		// TODO Auto-generated method stub
-		return false;
+		ContactEntity contactEntity = new ContactEntity();
+		BeanUtils.copyProperties(contact, contactEntity);
+		ContactEntity savedEntity = contactDtlRepository.save(contactEntity);
+		return savedEntity.getContactId()!=null;
 	}
 
 	@Override
